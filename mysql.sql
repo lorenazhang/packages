2d4d9488a20a6f0c8ebef2078e7bfc716d3e56e7
Jingyao Zhang <49734614+lorenazhang@users.noreply.github.com>
2026-06-25 16:56:35 -0400

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,17 +1,1 @@
-SELECT
 
-    DatabaseName,
-
-    TableName,
-
-    ColumnName
-
-FROM DBC.ColumnsV
-
-WHERE DatabaseName = 'YOUR_SCHEMA'
-
-  AND UPPER(ColumnName) = 'OPR_APPLICATION_NR'
-
-  AND TableName LIKE 'DLY_CR_%'
-
-ORDER BY TableName;
